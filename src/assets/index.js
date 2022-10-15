c29381348e338d3137081ee97822ed0f8c709088
Alejandro Fernandez Macias <afpato3@gmail.com>
2022-10-15 16:49:35 -0700

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import loader from './loader.svg';
+import vibes from './vibes.png';
+
+export {
+  loader,
+  vibes,
+};
---
+++ src/components/PlayPause.jsx
@@ -0,0 +1,18 @@
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+import SongBar from './SongBar';
+
+const PlayPause = ({ isPlaying, activeSong, handlePause, handlePlay }) => (isPlaying && activeSong?.title === SongBar.title ? (
+    <FaPauseCircle
+      size={35}
+      className='text-gray-300'
+      onClick={handlePause}
+      />
+  ) : (
+    <FaPlayCircle
+      size={35}
+      className='text-gray-300'
+      onClick={handlePlay}
+      />
+  ));
+
+export default PlayPause;
